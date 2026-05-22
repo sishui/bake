02e8aef2f65537682ce2aa9fe0934ec224510d23
sishui <sishuicn@gmail.com>
2026-05-22 13:04:02 +0800

    perf: writeFile

--- internal/codegen/run.go
+++ internal/codegen/run.go
@@ -243,8 +243,8 @@ func cleanDir(dir string) error {
 	})
 }
 
-func writeFile(ctx context.Context, dir string, filename string, data *bytes.Buffer) (string, error) {
-	fullPath := filepath.Join(dir, filename+".gen.go")
+func writeFile(ctx context.Context, dir string, filename string, data *bytes.Buffer) (fullPath string, err error) {
+	fullPath = filepath.Join(dir, filename+".gen.go")
 	file, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
 	if err != nil {
 		return "", err
@@ -253,20 +253,17 @@ func writeFile(ctx context.Context, dir string, filename string, data *bytes.Buf
 		if closeErr := file.Close(); closeErr != nil {
 			slog.ErrorContext(ctx, "close file", "file", fullPath, "error", closeErr)
 		}
-		if err == nil {
-			return
-		}
-		if removeErr := os.Remove(fullPath); removeErr != nil {
-			slog.ErrorContext(ctx, "remove file", "file", fullPath, "error", removeErr)
+		if err != nil {
+			if removeErr := os.Remove(fullPath); removeErr != nil {
+				slog.ErrorContext(ctx, "remove file", "file", fullPath, "error", removeErr)
+			}
 		}
 	}()
-	_, err = data.WriteTo(file)
-	if err != nil {
+	if _, err = data.WriteTo(file); err != nil {
 		return "", err
 	}
-	err = file.Sync()
-	if err != nil {
+	if err = file.Sync(); err != nil {
 		return "", err
 	}
-	return fullPath, err
+	return fullPath, nil
 }
